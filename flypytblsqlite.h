94df0e16d127e768d205fe1cf7b5957230fb6dc0
fanlumaster <1730976608@qq.com>
2023-04-04 12:36:52 +0800

    First commit. The first usable prototype of the project. This version has a severe flaw, that is, it would take over 800MB memory after initializing, because it load all ciku data to a map.

---
+++ flypytblsqlite.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "./libs/sqlite/sqlite3.h"
+
+std::unordered_map<std::string,
+                   std::vector<std::vector<std::pair<std::string, long>>>>
+transTableToMap(std::string dbPath, int pageSize);
