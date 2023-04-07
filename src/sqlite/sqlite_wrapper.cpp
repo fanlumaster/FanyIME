5261ac03bc9f8470adc0b1b4e16f940d14a55fa6
fanlumaster <1730976608@qq.com>
2023-04-07 21:32:11 +0800

    Optimize multi-word input.

--- src/sqlite/sqlite_wrapper.cpp
+++ src/sqlite/sqlite_wrapper.cpp
@@ -68,21 +68,61 @@ std::vector<std::pair<std::string, long>> queryTwoPinyin(sqlite3* db, std::strin
     return resVec;
 }
 
+/*
+    超过两个字的处理逻辑：
+        前两个字使用词库里面的双字词汇，
+        按照四个字符一组进行切分，
+        后面的拼音字符如果是三个，那么，仍然按照双字来查询，
+        最好凑不出三个字符的就使用单字查询
+
+        造出 16 个左右的词汇大概就可以了
+
+        TODO:
+        还可以再优化
+*/
 std::vector<std::pair<std::string, long>> queryMultiPinyin(sqlite3* db, std::string pinyin) {
     std::vector<std::pair<std::string, long>> resVec;
-    std::string tblName = "fullpinyinsimple";
-    std::string querySQL = "select * from " + tblName + " where key like " + "'" + pinyin + "%'" + " order by weight desc limit 200";
-    // std::cout << querySQL << '\n';
-    int result;
-    char* errMsg = nullptr;
-    int itemCount = 0;
-    UserData userData{itemCount, resVec};
-    // 查询
-    result = sqlite3_exec(db, querySQL.c_str(), queryPinyinCallback, &userData, &errMsg);
-    // std::cout << "itemCnt = " << itemCount << '\n';
-    if (result) {
-        // Todo: 日志
-        std::cout << "query error!" << '\n';
+    int splitBegin = 0;
+    std::string curPinyin;
+    std::vector<std::vector<std::pair<std::string, long>>> splitVecs;
+    std::vector<std::pair<std::string, long>> curVec;
+    int minSize = 16;  // 16 并没有什么特殊的含意，个人规定
+    while (splitBegin + 4 < pinyin.size()) {
+        curPinyin = pinyin.substr(splitBegin, 4);
+        curVec = queryTwoPinyin(db, curPinyin);
+        splitVecs.push_back(curVec);
+        splitBegin += 4;
+        if (curVec.size() < minSize) {
+            minSize = curVec.size();
+        }
+    }
+    if (splitBegin < pinyin.size()) {
+        curPinyin = pinyin.substr(splitBegin, 2);
+        curVec = queryPinyin(db, curPinyin);
+        splitVecs.push_back(curVec);
+        splitBegin += 2;
+        if (curVec.size() < minSize) {
+            minSize = curVec.size();
+        }
+    }
+    if (splitBegin < pinyin.size()) {
+        curPinyin = pinyin.substr(splitBegin, pinyin.size());  // 这里截取的只有一个字符了
+        curVec = queryPinyin(db, curPinyin);
+        splitVecs.push_back(curVec);
+        splitBegin += 2;
+        if (curVec.size() < minSize) {
+            minSize = curVec.size();
+        }
+    }
+    for (int i = 0; i < minSize; i++) {
+        std::pair<std::string, long> curPair;
+        std::string curStr = "";
+        long curWeight = 0;
+        for (auto eachVec : splitVecs) {
+            curStr += eachVec[i].first;
+            curWeight += eachVec[i].second;
+        }
+        resVec.push_back(std::make_pair(curStr, curWeight));
     }
     return resVec;
 }
